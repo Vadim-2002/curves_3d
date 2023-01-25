51daa185c937d67f472a956db6c284c5a0aceec7
Vadim-2002 <vadim.winter2@mail.ru>
2023-01-25 13:40:17 +0500

    Сделал небольшой рефакторинг и добавил пару функций

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <cstring>
 #include <ctime>
 #include <algorithm>
+#include <omp.h>
 
 
 class Curves
@@ -160,6 +161,38 @@ class Helix : public Circle
 };
 
 
+double sum_of_radii(std::vector<Curves*> curv_2)
+{
+    double sum_r = 0;
+    for (int i = 0; i < curv_2.size(); i++)
+        sum_r += ((Circle*)curv_2[i])->get_R();
+
+    return sum_r;
+}
+
+
+void output_of_points(std::vector<Curves*> curv, double t)
+{
+    for (int i = 0; i < curv.size(); i++)
+    {
+        std::cout << std::get<0> (curv[i]->get_point(t)) << "  ";
+        std::cout << std::get<1> (curv[i]->get_point(t)) << "  ";
+        std::cout << std::get<2> (curv[i]->get_point(t)) << "\n";
+    }
+}
+
+
+void output_of_first_derivatives(std::vector<Curves*> curv, double t)
+{
+    for (int i = 0; i < curv.size(); i++)
+    {
+        std::cout << std::get<0> (curv[i]->get_first_derivative(t)) << "  ";
+        std::cout << std::get<1> (curv[i]->get_first_derivative(t)) << "  ";
+        std::cout << std::get<2> (curv[i]->get_first_derivative(t)) << "\n";
+    }
+}
+
+
 int main()
 {
     std::vector<Curves*> curv;
@@ -173,34 +206,25 @@ int main()
     for (int i = 0; i < count_curves; i++)
     {
         if (rand() % 2 == 0)
-            curv.push_back(new Circle(i % 10));
+            curv.push_back(new Circle(rand() % 25));
         else
            if (rand() % 3 == 0)
-               curv.push_back(new Ellipse(rand() % 10, i % 10));
+               curv.push_back(new Ellipse(rand() % 20, i % 15));
            else
-               curv.push_back(new Helix(i + rand() % 10));
+               curv.push_back(new Helix(i + rand() % 15));
     }
 
     /*output of coordinates of points and first derivatives of all curves in the container*/
-    for (int i = 0; i < count_curves; i++)
-    {
-        std::cout << std::get<0> (curv[i]->get_point(t)) << ", " << std::get<1> (curv[i]->get_point(t)) << ", " << std::get<2> (curv[i]->get_point(t)) << "\n";
-        std::cout << std::get<0> (curv[i]->get_first_derivative(t)) << ", " << std::get<1> (curv[i]->get_first_derivative(t)) << ", " << std::get<2> (curv[i]->get_first_derivative(t)) << "\n";
-        std::cout << "\n";
-    }
+    output_of_points(curv, t);
+    std::cout << "\n\n";
+    output_of_first_derivatives(curv, t);
 
     /*filling the second container with okrazhnosty*/
     std::vector<Curves*> curv_2;
-    int count_curves_2 = 0;
     
-    for (int i = 0; i < count_curves; i++)
-    {
+    for (int i = 0; i < curv.size(); i++)
         if (curv[i]->isA("Circle"))
-        {
             curv_2.push_back(curv[i]);
-            count_curves_2++;
-        }
-    }
     
     /*sorting circles by increasing radius*/
     std::sort(curv_2.begin(), curv_2.end(), 
@@ -208,17 +232,13 @@ int main()
     );
     
     /*the sum of the radii of all circles from the second container*/
-    double sum_r = 0;
-    for (int i = 0; i < count_curves_2; i++)
-        sum_r += ((Circle*)curv_2[i])->get_R();
-
-    std::cout << sum_r << "\n";
+    std::cout << sum_of_radii(curv_2) << "\n";
     
     /*freeing up memory*/
-    for (int i = 0; i < count_curves; i++)
+    for (int i = 0; i < curv.size(); i++)
         delete curv[i];
 
-    for (int i = 0; i < count_curves_2; i++)
+    for (int i = 0; i < curv_2.size(); i++)
         delete curv_2[i];
 
     return 0;
